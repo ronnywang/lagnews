66ed9394fcba47e21f16ca6325a697f6c0a02555
Ronny Wang <ronnywang@gmail.com>
2013-12-23 11:31:42 +0800

    加上 json api 可以匯出所有頭條

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -5,4 +5,25 @@ class IndexController extends Pix_Controller
     public function indexAction()
     {
     }
+
+    public function jsonAction()
+    {
+        $ret = new StdClass;
+        $ret->data = array();
+        $time = null;
+        if (intval($_GET['before'])) {
+            $search = "`time` < " . intval($_GET['before']);
+        } else {
+            $search = '1';
+        }
+        foreach (HeadLineLog::search($search)->order('`time` DESC')->limit(30) as $log) {
+            $ret->data[] = json_decode($log->data);
+            $time = $log->time;
+        }
+        if (!is_null($time)) {
+            $ret->next_link = "http://{$_SERVER['HTTP_HOST']}/index/json?before={$time}";
+        }
+
+        return $this->json($ret);
+    }
 }
