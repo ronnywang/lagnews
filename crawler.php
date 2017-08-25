e2425c0e951af44f984f3accd59e2163671f4f88
Ronny Wang <ronnywang@gmail.com>
2017-08-25 23:15:35 +0800

    fb api 的 limit 改為最大 100 ，並且 created_time 改成要放入 fields 才會回傳

--- crawler.php
+++ crawler.php
@@ -19,7 +19,7 @@ class Crawler
         $access_token = getenv('FB_ACCESSTOKEN');
 
         $until = $timestamp + 86400;
-        $url = 'https://graph.facebook.com/148395741852581/feed?limit=200&until=' . $until . '&access_token=' . urlencode($access_token) . '&fields=message,link,picture';
+        $url = 'https://graph.facebook.com/148395741852581/feed?limit=100&until=' . $until . '&access_token=' . urlencode($access_token) . '&fields=message,link,picture,created_time';
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $json = json_decode(curl_exec($curl));
